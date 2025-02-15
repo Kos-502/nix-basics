911143f5d5cfba61f08be17afc5d7760019a9ab8
Kos-502 <designation.kos+github@gmail.com>
2025-02-16 00:37:46 +0200

    Learn nix data types

---
+++ data_types.nix
@@ -0,0 +1,17 @@
+{
+  some_attribute_set =
+  let
+    some_string = "Hello, world!";
+    some_integer = 1;
+    some_float = 1.5;
+    some_path = /home/kos;
+    some_list = [ 1 2 3 4 5 ];
+  in
+  {
+    my_string = some_string;
+    my_integer = some_integer;
+    my_float = some_float;
+    my_path = some_path;
+    my_list = some_list;
+  };
+}
